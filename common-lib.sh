be2f4785663f89a86023081319089e45d1d9ee70
elofturtle <jonatan@osninja.se>
2022-11-09 23:42:16 +0100

    A bit more to common lib

--- common-lib.sh
+++ common-lib.sh
@@ -1,9 +1,43 @@
+export bas="/opt/redeploy"
+
 function install_git {
-	sudo dnf -y install git
+	dnf -y install git
 }
 
 function install_caddy {
-	sudo dnf -y install 'dnf-command(copr)'
-	sudo dnf copr enable @caddy/caddy
-	sudo dnf -y install caddy
+	dnf -y install 'dnf-command(copr)'
+	dnf -y copr enable @caddy/caddy
+	dnf -y install caddy
+}
+
+function get_site_config {
+        if [[ -z "$www_target" ]]
+        then
+                source "$bas/redeploy.conf"
+        fi
+        if [[ ! -f "$1" ]]
+        then
+                :
+        else
+                source "$1"
+                repo_path="$www_target/$(tr '.' $'\n' <<< "$repo_fqdn" | tac | paste -s -d '/')"
+                if [[ -z "$sender" ]]
+                then
+                        sender="${2:-admin@${repo_fqdn}}"
+                fi
+
+                echo "${repo_fqdn} {
+                        root * ${repo_path}
+                        file_server
+                        tls ${sender}
+                }"
+        fi
+}
+
+function rootcheck {
+	if [[ "$(whoami)" != "root" ]]
+	then
+		echo "Please execute as root"
+		exit 1
+	fi
 }
