0f9c2cf7051074948b46a4a0e589f7cefdcf723d
elofturtle <jonatan@osninja.se>
2022-11-10 00:59:29 +0100

    we now assume root early on, so remove sudo from commands here

--- ubuntu-lib.sh
+++ ubuntu-lib.sh
@@ -1,11 +1,11 @@
 function install_git {
-	sudo apt -y install git
+	apt install -y git
 }
 
 function install_caddy {
-	sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+	apt install -y debian-keyring debian-archive-keyring apt-transport-https
 	curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 	curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-	sudo apt update
-	sudo apt  install -y caddy
+	apt update -y
+	apt  install -y caddy
 }
